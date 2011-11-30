b0e98467d169a24f5376958a9de0baca5215fcbc
askxuefeng <askxuefeng@localhost>
2011-11-30 13:36:01 +0800

    add multipart support

--- src/weibo.py
+++ src/weibo.py
@@ -49,28 +49,88 @@ class JsonObject(dict):
         self[attr] = value
 
 def _encode_params(**kw):
+    '''
+    Encode parameters.
+    '''
     args = []
     for k, v in kw.iteritems():
         qv = v.encode('utf-8') if isinstance(v, unicode) else v
         args.append('%s=%s' % (k, urllib.quote(qv)))
     return '&'.join(args)
 
+def _encode_multipart(**kw):
+    '''
+    A sample multipart/form-data; boundary=ABCD:
+
+--ABCD
+Content-Disposition: form-data; name="title"
+\r\n
+Today
+--ABCD
+Content-Disposition: form-data; name="1.txt"; filename="C:\1.txt"
+Content-Type: text/plain
+\r\n
+<file content of 1.txt>
+--ABCD--
+\r\n
+    '''
+    boundary = '----------%s' % hex(int(time.time() * 1000))
+    data = []
+    for k, v in kw.iteritems():
+        data.append('--%s' % boundary)
+        if hasattr(v, 'read'):
+            # file-like object:
+            ext = ''
+            filename = getattr(v, 'name', '')
+            n = filename.rfind('.')
+            if n != (-1):
+                ext = filename[n:].lower()
+            content = v.read()
+            data.append('Content-Disposition: form-data; name="%s"; filename="hidden"' % k)
+            data.append('Content-Length: %d' % len(content))
+            data.append('Content-Type: %s\r\n' % _guess_content_type(ext))
+            data.append(content)
+        else:
+            data.append('Content-Disposition: form-data; name="%s"\r\n' % k)
+            data.append(v.encode('utf-8') if isinstance(v, unicode) else v)
+    data.append('--%s--\r\n' % boundary)
+    return '\r\n'.join(data), boundary
+
+_CONTENT_TYPES = { '.png': 'image/png', '.gif': 'image/gif', '.jpg': 'images/jpeg', '.jpeg': 'images/jpeg', '.jpe': 'images/jpeg' }
+
+def _guess_content_type(ext):
+    return _CONTENT_TYPES.get(ext, 'application/octet-stream')
+
+_HTTP_GET = 0
+_HTTP_POST = 1
+_HTTP_UPLOAD = 2
+
 def _http_get(url, authorization=None, **kw):
-    return _http_call(url, True, authorization, **kw)
+    return _http_call(url, _HTTP_GET, authorization, **kw)
 
 def _http_post(url, authorization=None, **kw):
-    return _http_call(url, False, authorization, **kw)
+    return _http_call(url, _HTTP_POST, authorization, **kw)
 
-def _http_call(url, is_get, authorization, **kw):
+def _http_upload(url, authorization=None, **kw):
+    return _http_call(url, _HTTP_UPLOAD, authorization, **kw)
+
+def _http_call(url, method, authorization, **kw):
     '''
     send an http request and expect to return a json object if no error.
     '''
-    params = _encode_params(**kw)
-    http_url = '%s?%s' % (url, params) if is_get else url
-    http_body = None if is_get else params
+    params = None
+    boundary = None
+    if method==_HTTP_UPLOAD:
+        params, boundary = _encode_multipart(**kw)
+    else:
+        params = _encode_params(**kw)
+    http_url = '%s?%s' % (url, params) if method==_HTTP_GET else url
+    http_body = None if method==_HTTP_GET else params
     req = urllib2.Request(http_url, data=http_body)
     if authorization:
         req.add_header('Authorization', 'OAuth2 %s' % authorization)
+    if boundary:
+        req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
     resp = urllib2.urlopen(req)
     body = resp.read()
     r = json.loads(body, object_hook=_obj_hook)
@@ -80,15 +140,15 @@ def _http_call(url, is_get, authorization, **kw):
 
 class HttpObject(object):
 
-    def __init__(self, client, is_get):
+    def __init__(self, client, method):
         self.client = client
-        self.is_get = is_get
+        self.method = method
 
     def __getattr__(self, attr):
         def wrap(**kw):
             if self.client.is_expires():
                 raise APIError('21327', 'expired_token', attr)
-            return _http_call('%s%s.json' % (self.client.api_url, attr.replace('__', '/')), self.is_get, self.client.access_token, **kw)
+            return _http_call('%s%s.json' % (self.client.api_url, attr.replace('__', '/')), self.method, self.client.access_token, **kw)
         return wrap
 
 class APIClient(object):
@@ -104,8 +164,9 @@ class APIClient(object):
         self.api_url = 'https://%s/%s/' % (domain, version)
         self.access_token = None
         self.expires = 0.0
-        self.get = HttpObject(self, True)
-        self.post = HttpObject(self, False)
+        self.get = HttpObject(self, _HTTP_GET)
+        self.post = HttpObject(self, _HTTP_POST)
+        self.upload = HttpObject(self, _HTTP_UPLOAD)
 
     def set_access_token(self, access_token, expires_in):
         self.access_token = str(access_token)
@@ -127,8 +188,7 @@ class APIClient(object):
                 client_secret = self.client_secret, \
                 redirect_uri = self.redirect_uri, \
                 code = code, grant_type = 'authorization_code')
-        print r, type(r)
-        self.set_access_token(r.access_token, r.expires_in)
+        return r
 
     def is_expires(self):
         return not self.access_token or time.time() > self.expires
